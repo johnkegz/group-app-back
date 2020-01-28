9f396bd50ab39c9ce52a3f5548d08a50119b3839
johnkegz <johnkal24@gmail.com>
2020-01-28 12:02:09 +0300

    add sockect.io

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,55 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    res.send("hello world");
+"use strict";
+// const express = require('express')
+// const socketIo   = require('socket.io');
+// const path = require('path');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const app = express();
+// app.get('/', (req: any, res: any) => {
+//     // res.send("hello world")
+//     res.sendFile(path.resolve("./client/index.html"));
+// })
+// const server = app.listen(3000, () => {
+//     console.log("App run at 3000")
+// })
+// const webSocket = socketIo(server); //initiate socket
+// webSocket.on("channel1", (data: any)=>{
+//     console.log("channel 1", data)
+// })
+// webSocket.emit("channel 2", "new channel");
+// webSocket.on("channel2", (obj: any)=>{
+//     console.log("channel 2", obj)
+// })
+const express_1 = __importDefault(require("express"));
+const path = __importStar(require("path"));
+const app = express_1.default();
+app.set("port", process.env.PORT || 3000);
+let http = require("http").Server(app);
+// set up socket.io and bind it to our
+// http server.
+let io = require("socket.io")(http);
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./client/index.html"));
 });
-app.listen(3000, () => {
-    console.log("App run at 3000");
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+io.on("connection", function (socket) {
+    console.log("a user connected");
+    socket.on("message", function (message) {
+        console.log(message);
+        socket.emit("message", message);
+    });
+});
+const server = http.listen(3000, function () {
+    console.log("listening on *:3000");
 });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,11 +1,58 @@
-const express = require('express')
+// const express = require('express')
+// const socketIo   = require('socket.io');
+// const path = require('path');
+
+// const app = express();
+
+// app.get('/', (req: any, res: any) => {
+//     // res.send("hello world")
+//     res.sendFile(path.resolve("./client/index.html"));
+// })
+
+// const server = app.listen(3000, () => {
+//     console.log("App run at 3000")
+// })
+
+// const webSocket = socketIo(server); //initiate socket
+
+// webSocket.on("channel1", (data: any)=>{
+//     console.log("channel 1", data)
+// })
+
+// webSocket.emit("channel 2", "new channel");
+
+// webSocket.on("channel2", (obj: any)=>{
+//     console.log("channel 2", obj)
+// })
+
+
+import express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";
 
 const app = express();
+app.set("port", process.env.PORT || 3000);
 
-app.get('/', (req: any, res: any) => {
-    res.send("hello world")
-})
+let http = require("http").Server(app);
+// set up socket.io and bind it to our
+// http server.
+let io = require("socket.io")(http);
 
-app.listen(3000, () => {
-    console.log("App run at 3000")
-})
+app.get("/", (req: any, res: any) => {
+  res.sendFile(path.resolve("./client/index.html"));
+});
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+io.on("connection", function(socket: any) {
+  console.log("a user connected");
+  socket.on("message", function(message: any) {
+    console.log(message);
+    socket.emit("message", message);
+  });
+  
+});
+
+const server = http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
